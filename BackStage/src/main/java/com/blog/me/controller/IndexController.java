bc596f2e7a3b4ff2f80194bcf43f2041b8b9cd13
minxiang <294315568@qq.com>
2018-12-27 17:04:57 +0800

    project init

---
+++ BackStage/src/main/java/com/blog/me/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.blog.me.controller;
+
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping(value = "/index.html")
+    @ResponseBody
+    public String indexPage(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code",200);
+        jsonObject.put("msg","响应成功");
+
+        return jsonObject.toString();
+    }
+}
