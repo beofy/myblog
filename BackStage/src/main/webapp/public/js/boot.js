c5e6e8361ac69b9fd56a397411c03b1fcceb7c39
minxiang <294315568@qq.com>
2018-12-28 15:05:03 +0800

    配置require，js入口文件

---
+++ BackStage/src/main/webapp/public/js/boot.js
@@ -0,0 +1,12 @@
+requirejs.config({
+    //By default load any module IDs from js/lib
+    //baseUrl: 'public/js',
+    paths: {
+        jqery:'lib/jquery',
+        layui:"layui/layui",
+    }
+
+
+});
+
+//require(['layui.config']);
