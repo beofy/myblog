ab99d8b4101b23b9de606afc58d594c3f7a55b97
minxiang <294315568@qq.com>
2018-12-30 02:28:29 +0800

    常量类

---
+++ BackStage/src/main/java/com/blog/me/common/Constants/SecurityKey.java
@@ -0,0 +1,10 @@
+package com.blog.me.common.Constants;
+
+public final class SecurityKey {
+
+    private final static String PRIVATEKEY="JdBuIIIoAfuqcPJkNOFAfRDLqDO3hl247Ao/mBooLkRyFT0Bfh14aeHcWkf4rtIu/rmUxHsSxhkrHfzWrtSi1EinplbIgcr2IAW/0ywjEolwp4mpXGDhUh8atuA161rgeK4IV8A2oGfh/V4DsHXr/QOZ8nbFIESuHp/I7qGVDuk=";
+
+    private final static String PUBLICKEY= "RyoASTxXi91SYvVloCLTK7OsTya9VNalG4qOsdLvP0rNYzcfXA5YLM2sCdptdVXi0cK2wLhLJQFT2HjnU+fBKg1/VpBsQvZTbIuxlkRXas7Rc3zEO6GFtIjQuJcS9pvo4rOUIkSY67pRezHHZkoAwxnGQ8/Pt5Z9Qql7OlTHRHw=";
+
+    private final static String MD5_KEY="7uAWkt4MbcoZHHsS";
+}
