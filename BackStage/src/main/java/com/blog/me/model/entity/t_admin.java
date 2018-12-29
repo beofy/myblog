a767ce657f6e672ff9293d6b901c38129cdb5490
minxiang <294315568@qq.com>
2018-12-30 02:28:13 +0800

    数据库实体类

---
+++ BackStage/src/main/java/com/blog/me/model/entity/t_admin.java
@@ -0,0 +1,66 @@
+package com.blog.me.model.entity;
+
+import java.util.Date;
+
+public class t_admin {
+
+    private Long id;
+
+    private Date create_time;
+
+    private String account;
+
+    private String password;
+
+    private Date last_login_time;
+
+    private String last_login_ip;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getLast_login_time() {
+        return last_login_time;
+    }
+
+    public void setLast_login_time(Date last_login_time) {
+        this.last_login_time = last_login_time;
+    }
+
+    public String getLast_login_ip() {
+        return last_login_ip;
+    }
+
+    public void setLast_login_ip(String last_login_ip) {
+        this.last_login_ip = last_login_ip;
+    }
+}
