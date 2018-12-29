1a8754691368811e79fb83dd7ac0a9cbfdf344c7
minxiang <294315568@qq.com>
2018-12-29 18:22:02 +0800

    update

--- BackStage/src/main/webapp/public/js/boot.js
+++ BackStage/src/main/webapp/public/js/boot.js
@@ -2,11 +2,16 @@ requirejs.config({
     //By default load any module IDs from js/lib
     //baseUrl: 'public/js',
     paths: {
-        jqery:'lib/jquery',
+        jquery:'lib/jquery',
         layui:"layui/layui",
+        "crypto-js":"encrypt/crypto-js/core",//des加密工具
+        encrypt:"encrypt/encrypt"//rsa工具
+    },
+    shim:{
+
+
     }
 
 
 });
 
-//require(['layui.config']);
